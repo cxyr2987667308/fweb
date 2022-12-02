1541ffb4c6389b79f1dd01698b17b31171f90c83
lj.fang <lj.fang@zuolin.com>
2022-12-02 18:17:52 +0800

    修改样式

--- qiankun-base/src/App.js
+++ qiankun-base/src/App.js
@@ -2,10 +2,10 @@
  * @Author: liF
  * @Date: 2022-09-19 15:04:17
  * @LastEditors: liF
- * @LastEditTime: 2022-12-02 17:24:43
+ * @LastEditTime: 2022-12-02 18:15:33
  */
 import React, { useEffect } from 'react';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Link } from 'react-router-dom';
 import { registerMicroApps, start, initGlobalState } from 'qiankun';
 import { ConfigProvider } from 'antd';
 import zhCN from 'antd/es/locale/zh_CN';
@@ -18,7 +18,7 @@ const App = () => {
     // 微应用地址，子应用必须支持跨域 fetch
     entry: '//localhost:8081',
     // 微应用挂载的容器节点
-    container: '#react',
+    container: '#appContainer',
     // 微应用的激活规则，访问到react的时候跳转子应用
     activeRule: '#/react',
     // 主应用需要传递给微应用的数据
@@ -36,12 +36,11 @@ const App = () => {
   <ConfigProvider locale={zhCN}>
     <Router>
       <div>
-        <div>4444455</div>
-        <Switch>
-          <Route path="/react" element={<div id="react6"></div>} />
-        </Switch>
-
-        <div id="react"></div>
+        <div className='app-menu'>
+          <Link to='/react'>react</Link>
+          <Link to='/vue'>vue</Link>
+        </div>
+        <div id="appContainer"></div>
       </div>
     </Router>
   </ConfigProvider>
--- qiankun-react/src/index.js
+++ qiankun-react/src/index.js
@@ -1,10 +1,16 @@
+/*
+ * @Author: liF
+ * @Date: 2022-11-17 20:26:21
+ * @LastEditors: liF
+ * @LastEditTime: 2022-12-02 18:08:24
+ */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('appRoot'));
 root.render(
   <React.StrictMode>
     <App />
