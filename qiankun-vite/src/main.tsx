9d853ae0c215ff4317c8048410d912adc2d365d3
fanglijiao <lj.fang@zuolin.com>
2023-11-09 17:43:03 +0800

    增加vite

--- qiankun-vite/src/main.tsx
+++ qiankun-vite/src/main.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ConfigProvider } from 'antd'
+import packageJson from './../package.json';
 import App from './App.tsx'
 import './index.css'
 
+const prefixCls = packageJson.antdConfig.prefixCls;
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ConfigProvider prefixCls={prefixCls}>
+      <App />
+    </ConfigProvider>
   </React.StrictMode>,
 )
--- qiankun-vite/vite.config.ts
+++ qiankun-vite/vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+const packageJson = require('./package.json');
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      less: {
+        modifyVars: {
+          'ant-prefix': packageJson.antdConfig.prefixCls
+        },
+        // 支持内联 Javascript
+        javascriptEnabled: true,
+      },
+      module: true
+    }
+  }
 })
