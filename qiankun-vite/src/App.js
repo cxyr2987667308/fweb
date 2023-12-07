ff52bb34bd07353bbc1714ea864295528d24aab6
fanglijiao <lj.fang@zuolin.com>
2023-12-07 16:45:30 +0800

    去掉没用的

--- qiankun-vite/src/App.js
+++ qiankun-vite/src/App.js
@@ -1,13 +1,10 @@
-import React, { useState, lazy } from 'react';
+import React, { lazy } from 'react';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.less'
-// import Home from './routes/home';
+import './App.less';
 const Home = lazy(() => import('./routes/home'));
 const Login = lazy(() => import('./routes/login'));
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <Router basename={window.__POWERED_BY_QIANKUN__ ? '/react' : '/'}>
       <div>
